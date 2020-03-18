c22c1624e704af35d0eb5edbc280fee0f8c6d08e
rvn9 <56660530+rvn9@users.noreply.github.com>
2020-03-18 11:54:02 +0700

    Update Caesar Cipher.cpp

--- Caesar Cipher.cpp
+++ Caesar Cipher.cpp
@@ -1,3 +1,7 @@
+// CREATED BY ANDREAS AGUSTINUS //
+// Contact person: Andreasagustinus11@gmail.com //
+// FEEL FREE TO USE THE CODE, FOR EDUCATIONAL PURPOSE //
+
 #include <mpi.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
@@ -14,76 +18,93 @@ char caesar(char msg, int k) {
         return char(int(msg + k - 97) % 26 + 97);
     }
 }
+//UNTUK TERIMA INPUT DARI USER 
+char plaintext[1000]; 
 
-// Temporary array for slave process 
-char plaintext[1000];
-char a2[1000]; // UNTUK NYIMPEN CHARACTER YG MESTI DI ENKRIP DI SLAVE //
-char a3[1000]; // UNTUK NYIMPEN CHARACTER HASIL ENKRIPSI DI ROOT (MASTER) //
-char chipertext_master[1000];
-char chipertext_slave[1000];
+// UNTUK MENYIMPAN CHARACTER YG AKAN DI KIRIM, UNTUK DI ENCODE DI SETIAP SLAVE //
+char placeholder_slave[1000]; 
+
+//MENYIMPAN CHARACTER HASIL ENKRIPSI DARI SETIAP SLAVE KE DI ROOT (MASTER),
+//UNTUK DI KIRIM KE ROOT (MASTER) 
+char placeholder_master[1000]; 
+
+// MENAMPUNG HASAIL ENKRIPSI MASTER  //
+char chipertext_master[1000]; 
+
+// MENAMPUNG HASAIL ENKRIPSI SLAVE  //
+char chipertext_slave[1000]; 
+
+// N = JUMLAH DATA, SHIFT = BANYAKNYA PERGESERAN KARAKTER //
 int n, shift;
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    string hasil;
-    int pid, np,
+    int rank, size,
         elements_per_process,
         n_elements_recieved,
         terima_slave;
 
-    // np -> no. of processes 
-    // pid -> process id 
+    // SIZE -> BANYAKNYA PROCESS
+    // RANK -> PROCESS ID
     double elapsed_time;
     MPI_Status status;
 
-    // Creation of parallel processes 
+    // INISIALISASI MPI
     MPI_Init(&argc, &argv);
 
     // find out process ID, 
     // and how many processes were started 
-    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+   
     elapsed_time = -MPI_Wtime();
+
     // master process 
-    if (pid == 0) {
+    if (rank == 0) {
         int index, i;
 
         do {
+            // MASUKAN PLAINTEXT //
             std::cout << "Masukkan Kata : ";
-            std::cin >> plaintext; // MASUKAN PLAINTEXT //
+            std::cin >> plaintext; 
             fflush(stdout);
 
 
+            // MASUKAN SHIFT //
             std::cout << "Masukan jumlah shift : ";
             std::cin >> shift;
             fflush(stdout);
 
-
+            // MENGAMBIL PANJANG PLAINTEXT//
             n = strlen(plaintext);
-            elements_per_process = n / np;
-            if (n < np) {
+
+            // MEMBAGI TASK YANG HARUS DI KERJAKAN PER PROCESSOR //
+            elements_per_process = n / size;
+
+            // ERROR HANDLING APABILA JUMLAH KARAKTER KURANAG DARI JUMLAH PROCESSOR, PROCESS YG DI INPUT TERLALU BANYAK// 
+            if (n < size) {
                 printf("Jumlah Karakter kurang dari jumlah processor, Silakan input ulang kata\n\n");
             }
             
-        } while (n < np);
-       
-        // check if more than 1 processes are run 
-        
+        } while (n < size);
     }
 
+    // BROADCAST KE SEMUA PROCESSOR NILAI SHIFT / PERGESERAN KARAKTER //
     MPI_Bcast(&shift, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // BROADCAST PANJANG PLAINTEXT KE SEMUA PROCESSOR //
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (pid == 0) {
+    if (rank == 0) {
         int index, i;
-        if (np > 1) {
-            // distributes the portion of array 
-            // to child processes
-            for (i = 1; i < np - 1; i++) {
+        if (size > 1) {
+            // DISTRIBUSI TASK KE SLAVE
+            for (i = 1; i < size - 1; i++) {
                 index = i * elements_per_process;
-
+                // KOMUNIKASI DARI MASTER KE SLAVE
                 MPI_Send(&elements_per_process,
                     1, MPI_INT, i, 0,
                     MPI_COMM_WORLD);
@@ -93,10 +114,11 @@ int main(int argc, char* argv[])
                     MPI_COMM_WORLD);
             }
 
-            // last process adds remaining elements 
+            // TASK SISA YANG HARUS DI KERJAKAN OLEH SLAVE PROCESSOR //
             index = i * elements_per_process;
             int elements_left = n - index;
 
+            // KOMUNIKASI DARI MASTER KE SLAVE
             MPI_Send(&elements_left,
                 1, MPI_INT,
                 i, 0,
@@ -109,33 +131,29 @@ int main(int argc, char* argv[])
 
         // encrypt master process
         for (i = 0; i < elements_per_process; i++) {
-            printf("processor %d ada data %c\n", pid, plaintext[i]);
+            printf("processor %d ada data %c\n", rank, plaintext[i]);
             chipertext_master[i] = caesar(plaintext[i], shift);
         }
-
-
         printf("MASTER : %s", chipertext_master);
-
-
     }
+
+
     // slave processes 
     else {
+        // MENERIMA KIRIMAN DARI MASTER
         MPI_Recv(&n_elements_recieved,
             1, MPI_INT, 0, 0,
             MPI_COMM_WORLD,
             &status);
-
-        // stores the received array segment 
-        // in local array a2 
-        MPI_Recv(&a2, n_elements_recieved,
+        MPI_Recv(&placeholder_slave, n_elements_recieved,
             MPI_CHAR, 0, 0,
             MPI_COMM_WORLD,
             &status);
 
-
+        // SLAVE MELAKUKAN ENKRIPSI DARI SETIAP TASK (CHARACTER) YANG IA DAPAT //
         for (int i = 0; i < n_elements_recieved; i++) {
-            printf("processor %d ada data %c\n", pid, a2[i]);
-            chipertext_slave[i] = caesar(a2[i], shift);
+            printf("processor %d ada data %c\n", rank, placeholder_slave[i]);
+            chipertext_slave[i] = caesar(placeholder_slave[i], shift);
         }
 
         // NGE SEND hasil enkripsi slave ke master // 
@@ -143,45 +161,39 @@ int main(int argc, char* argv[])
             n_elements_recieved,
             MPI_CHAR, 0, 99,
             MPI_COMM_WORLD);
-
-
         MPI_Send(&n_elements_recieved,
             1, MPI_INT,
             0, 0,
             MPI_COMM_WORLD);
 
-
         printf("SLAVE : %s\n\n", chipertext_slave);
 
-
     }
-    // PRINT TIME //
-    if (pid == 0) {
+
+    // PRINT HASIL & WAKTU YG DIBUTUHKAN //
+    if (rank == 0) {
         elapsed_time += MPI_Wtime();
         // printf("Hasil encoding adalah %s", hasil);
         printf("\n\nTotal elapsed time: %10.6f\n", elapsed_time);
 
-        // INI Ngerecieve semua enkripsi dari setiap slave processor //
-        for (int i = 1; i < np;i++) {
+        // INI Nge-recieve semua enkripsi dari setiap slave processor //
+        for (int i = 1; i < size;i++) {
             MPI_Recv(&terima_slave,
                 1, MPI_INT, MPI_ANY_SOURCE, 0,
                 MPI_COMM_WORLD,
                 &status);
 
-            MPI_Recv(&a3, terima_slave,
+            MPI_Recv(&placeholder_master, terima_slave,
                 MPI_CHAR, MPI_ANY_SOURCE, 99,
                 MPI_COMM_WORLD,
                 &status);
 
-            strcat_s(chipertext_master, sizeof(a3) ,a3);
+            // CONCAT HASIL ENRKIPSI DARI SLAVE PROCESS //
+            strcat_s(chipertext_master, sizeof(placeholder_master) , placeholder_master);
 
         }
-
-
         // GABUNGIN HASIL ENKRIPSI SLAVE SAMA MASTER //
-        
         printf("Hasil encode : %s \n", chipertext_master);
-
     }
 
     // cleans up all MPI state before exit of process 
